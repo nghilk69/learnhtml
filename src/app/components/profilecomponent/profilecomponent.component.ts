d4b56760b55937c324c472dc50e62fb6b40fcbe2
phamj nghị <nghilk@ql6625.fr>
2022-04-22 17:43:12 +0700

    init

---
+++ src/app/components/profilecomponent/profilecomponent.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-profilecomponent',
+  templateUrl: './profilecomponent.component.html',
+  styleUrls: ['./profilecomponent.component.css']
+})
+export class ProfilecomponentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
